ea4304e0a8d426899acea6bf925bd67e8c32c66f
levaid <levaid@vivaldi.net>
2019-12-17 15:02:50 +0100

    stats working 100

--- full_test.py
+++ full_test.py
@@ -30,6 +30,7 @@ import random
 import os  # handy system and path functions
 import sys  # to get file system encoding
 import pandas as pd
+from stats import do_statistics
 
 
 from psychopy.hardware import keyboard
@@ -1196,64 +1197,7 @@ with open(filename + '_commentary.txt', 'w') as f:
         f.write(key + '\n' + value + '\n')
 
 try:
-    writer = pd.ExcelWriter(filename+'_data.xlsx')
-    raw_output = pd.read_csv(filename+'.csv', engine='python', encoding='utf-8-sig')
-    raw_output.to_excel(writer, 'sheet1', index=False)
-    writer.save()
-except Exception as e:
-    print('Couldn\'t generate xlsx from csv')
-    print(e)
-
-try:
-    if len(LIST_OF_KEYS) == len(stimuli):
-        output_tables = []
-        table = pd.read_excel(filename+'_data.xlsx')
-        table_sentence = table[(table['stimulus_type'] == 'sentence')].rename(columns={'stimulus_plaus':'sentences_stimulus_plaus', 'word_order':'sentences_word_order'})
-        table_image = table[(table['stimulus_type'] == 'image')].rename({'stimulus_plaus':'images_stimulus_plaus'})
-        output_tables.append(pd.crosstab([table['stimulus_type'],table['stimulus_plaus']],table['answer_role'], margins = True))
-
-        output_tables += [table.groupby(['stimulus_plaus'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-
-        output_tables += [table_sentence.groupby(['sentences_word_order'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-
-        output_tables += [table_sentence.groupby(['sentences_word_order', 'sentences_stimulus_plaus'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-                        
-        output_tables += [table.groupby(['stimulus_type'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-        output_tables += [table.groupby(['stimulus_type', 'stimulus_plaus'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-        output_tables += [table.groupby(['answer_role'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-        output_tables += [table.groupby(['nom1_indented'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-        output_tables += [table.groupby(['nom2_indented'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-        output_tables += [table.groupby(['key_resp.keys'], as_index=False).agg(
-                        {'key_resp.rt':['mean','std', 'min', 'max']})]
-
-        # BUG in old pandas, you can't normalize by multiindex
-        # output_tables.append(pd.crosstab([table['stimulus_type'],table['stimulus_plaus']],table['answer_role'], margins = True, normalize='index'))
-        output_tables.append(pd.crosstab(table['stimulus_plaus'],table['answer_role'], margins = True, normalize='index'))
-
-        output_tables.append(pd.crosstab(table_sentence['sentences_stimulus_plaus'],table_sentence['answer_role'], margins = True))
-        test = pd.crosstab(table_sentence['sentences_word_order'],table_sentence['answer_role'], margins = True)
-        output_tables.append(test)
-        #output_tables.append(pd.crosstab(table_image['stimulus_plaus'],table_image['answer_role'], margins = True))
-        output_tables.append(pd.crosstab(table['key_resp.keys'],table['answer_role'], margins = True))
-        output_tables.append(pd.crosstab(table['nom1_indented'],table['key_resp.keys'], margins = True))
-        output_tables.append(pd.crosstab(table['nom2_indented'],table['key_resp.keys'], margins = True))
-
-
-        writer = pd.ExcelWriter(filename+'_statistics.xlsx')
-        offset = 0
-        for dataframe in output_tables:
-            # print(dataframe)
-            dataframe.to_excel(writer, "sheet1", index=True, startrow=offset)
-            offset += len(dataframe) + 5
-        writer.save()
+    do_statistics(filename)
 except Exception as e:
     print('cannot generate statistics')
     print(e)
